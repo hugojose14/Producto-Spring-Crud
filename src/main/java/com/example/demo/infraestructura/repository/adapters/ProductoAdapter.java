c358c182cb0b6be42f9fb07db9383857124f8246
hugojose14 <hugo.jose.1@hotmail.com>
2019-12-14 15:38:49 -0500

    Adapter

--- src/main/java/com/example/demo/infraestructura/repository/adapters/ProductoAdapter.java
+++ src/main/java/com/example/demo/infraestructura/repository/adapters/ProductoAdapter.java
@@ -2,31 +2,38 @@ package com.example.demo.infraestructura.repository.adapters;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.dominio.model.Producto;
 import com.example.demo.dominio.services.ProductoService;
+import com.example.demo.infraestructura.mapper.ProductoMapper;
+import com.example.demo.infraestructura.repository.database.ProductoRepository;
 import com.example.demo.shared.dominio.Id;
 
 @Service
 public class ProductoAdapter implements ProductoService{
-
+	
+	@Autowired
+	public ProductoRepository productoRepository;
+	
+	@Autowired
+	public ProductoMapper productoMapper;
+	
 	@Override
 	public List<Producto> buscarPorIds(List<Id> id) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public Producto guardar(Producto producto) {
-		// TODO Auto-generated method stub
-		return null;
+	public void guardar(Producto producto) {
+		productoRepository.save(productoMapper.DomToDto(producto));
+		
 	}
 
 	@Override
 	public Producto buscarPorId(Id id) {
-		// TODO Auto-generated method stub
-		return null;
+		
 	}
 	
 	
